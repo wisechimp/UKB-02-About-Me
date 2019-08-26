d74b2d689c9c0141ded816b63314440cc7694bca
shicks1080 <stuart.hicks@gmail.com>
2019-08-26 13:50:23 +0200

    DataBinding added
    Replaced the Kotlin Extention references with DataBinding instead

--- app/src/main/java/com/wisechimp/aboutme/MainActivity.kt
+++ app/src/main/java/com/wisechimp/aboutme/MainActivity.kt
@@ -4,24 +4,34 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.wisechimp.aboutme.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var userName: ProfileData
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        userName = ProfileData(getString(R.string.subjects_name))
+        binding.userName = userName
 
-        profile_nickname_butt.setOnClickListener {
+        binding.profileNicknameButt.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        profile_nickname_tv.text = profile_nickname_edit_text.text
-        profile_nickname_edit_text.visibility = View.GONE
-        view.visibility = View.GONE
-        profile_nickname_tv.visibility = View.VISIBLE
+        binding.apply {
+            userName?.nickname = profile_nickname_edit_text.text.toString()
+            invalidateAll()
+            profile_nickname_edit_text.visibility = View.GONE
+            view.visibility = View.GONE
+            profile_nickname_tv.visibility = View.VISIBLE
+        }
 
         //Hide the keyboard
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
---
+++ app/src/main/java/com/wisechimp/aboutme/ProfileData.kt
@@ -0,0 +1,7 @@
+package com.wisechimp.aboutme
+
+data class ProfileData(
+    var name : String = "",
+    var nickname: String = "",
+    var bio: String = ""
+)
