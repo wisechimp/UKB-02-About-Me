edc86d122cdeb9948563181dbeb6ef9f5d0a5fb5
shicks1080 <stuart.hicks@gmail.com>
2019-08-26 12:15:11 +0200

    Adding nickname functionality
    An editText for users to set a nickname plus a button to save this into a separate textView. Used the kotlin android extentions rather than findViewById.

--- app/src/main/java/com/wisechimp/aboutme/MainActivity.kt
+++ app/src/main/java/com/wisechimp/aboutme/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.wisechimp.aboutme
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profile_nickname_butt.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        profile_nickname_tv.text = profile_nickname_edit_text.text
+        profile_nickname_edit_text.visibility = View.GONE
+        view.visibility = View.GONE
+        profile_nickname_tv.visibility = View.VISIBLE
+
+        //Hide the keyboard
+        val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
